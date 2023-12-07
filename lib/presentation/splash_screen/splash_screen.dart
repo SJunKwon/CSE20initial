681948c4a845888ea35c883e09076f35f8e85ffc
robymarManaloto <manaloto.robymarlouise@auf.edu.ph>
2023-12-08 00:02:13 +0800

    clearscreen screen responsive

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: unused_local_variable
-
 import 'package:flutter/material.dart';
 import 'package:facetap/core/app_export.dart';
 import 'package:facetap/widgets/custom_elevated_button.dart';
@@ -9,65 +7,72 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaQueryData = MediaQuery.of(context);
-
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          width: double.maxFinite,
-          padding: EdgeInsets.symmetric(horizontal: 45.h),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CustomImageView(
-                imagePath: ImageConstant.imgFaceTrackerPrimary,
-                height: 192.v,
-                width: 150.h,
-                margin: EdgeInsets.only(bottom: 47.v),
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Face".toUpperCase(),
-                      style: theme.textTheme.displaySmall,
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            final screenHeight = constraints.maxHeight;
+            final screenWidth = constraints.maxWidth;
+
+            return Container(
+              width: double.maxFinite,
+              padding: EdgeInsets.symmetric(horizontal: 0.1 * screenWidth),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CustomImageView(
+                    imagePath: ImageConstant.imgFaceTrackerPrimary,
+                    height: 0.4 * screenHeight,
+                    width: 0.3 * screenWidth,
+                    margin: EdgeInsets.only(bottom: 0.01 * screenHeight),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Face".toUpperCase(),
+                          style: theme.textTheme.displaySmall,
+                        ),
+                        TextSpan(
+                          text: "TAP".toUpperCase(),
+                          style: CustomTextStyles.displaySmallPrimary,
+                        ),
+                      ],
                     ),
-                    TextSpan(
-                      text: "TAP".toUpperCase(),
-                      style: CustomTextStyles.displaySmallPrimary,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 0.005 * screenHeight),
+                  Container(
+                    width: 0.7 * screenWidth, // Adjust the width as needed
+                    margin: EdgeInsets.symmetric(horizontal: 0.038 * screenWidth),
+                    child: Text(
+                      "FaceTap is a facial recognition app designed to simplify and streamline attendance management for educational institutions, corporate organizations, and events using facial recognition technology.",
+                      maxLines: 5,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                      style: CustomTextStyles.bodySmall10.copyWith(height: 1.84),
                     ),
-                  ],
-                ),
-                textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 0.03 * screenHeight),
+                  Container(
+                    width: 0.7 * screenWidth, // Adjust the width as needed
+                    child: CustomElevatedButton(
+                      height: 0.044 * screenHeight,
+                      text: "Get Started".toUpperCase(),
+                      margin: EdgeInsets.only(left: 0.017 * screenWidth),
+                      buttonStyle: CustomButtonStyles.outlinePrimaryBL4,
+                      buttonTextStyle: CustomTextStyles.labelLargeOnPrimaryContainerBlack,
+                      onPressed: () {
+                        onTapGetStarted(context);
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 0.005 * screenHeight),
+                ],
               ),
-              SizedBox(height: 2.v),
-              Container(
-                width: 210.h,
-                margin: EdgeInsets.only(left: 38.h, right: 20.h),
-                child: Text(
-                  "facetap is a facial recognition app designed to simplify and streamline attendance management for educational institutions, corporate organizations, and events using facial recognition technology.",
-                  maxLines: 5,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: CustomTextStyles.bodySmall10.copyWith(height: 1.84),
-                ),
-              ),
-              SizedBox(height: 30.v),
-              CustomElevatedButton(
-                height: 44.v,
-                text: "Get Started".toUpperCase(),
-                margin: EdgeInsets.only(left: 17.h),
-                buttonStyle: CustomButtonStyles.outlinePrimaryBL4,
-                buttonTextStyle:
-                CustomTextStyles.labelLargeOnPrimaryContainerBlack,
-                onPressed: () {
-                  onTapGetStarted(context);
-                },
-              ),
-              SizedBox(height: 5.v),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
